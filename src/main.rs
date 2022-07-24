fd5110b89664421768a146ad8eb2d28281f68703
Geir Sagberg <geir.sagberg@gmail.com>
2022-07-24 15:53:26 +0200

    Idle animation working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(animate_sprites)
+        .run();
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn setup(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = assets.load("cyborg/Idle1.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(48., 48.), 4, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            ..default()
+        })
+        .insert(AnimationTimer(Timer::from_seconds(0.1, true)));
+}
+
+fn animate_sprites(
+    time: Res<Time>,
+    texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
 }
